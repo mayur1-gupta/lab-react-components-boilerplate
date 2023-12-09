6d7112206cf5f2f45b88eaaea1fdc3eec555b6f9
Mayur Gupta <mayurgupta@Mayurs-MacBook-Air.local>
2023-12-09 11:34:59 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,36 @@
 import './App.css';
-import lake from "./image/lake.JPG";
+// import lake from "./image/lake.JPG";
 import GallaryFooter from './components/GallaryFooter';
-
+import GallaryHeader from './components/GallaryHeader';
+import GallaryBody from './components/GallaryBody';
+// import imageData from './components/lakeIamage';
+import lake from "./image/lake.JPG";
 const imageData = [
-  {
-    id:1,
-    img:lake 
-  },
-  {
-    id:2,
-    img:lake
-  },
-  {
-    id:3,
-    img:lake
-  },
-  {
-    id:4,
-    img:lake
-  }
-]
+    {
+      id:1,
+      img:lake 
+    },
+    {
+      id:2,
+      img:lake
+    },
+    {
+      id:3,
+      img:lake
+    },
+    {
+      id:4,
+      img:lake
+    }
+  ]
+
 
 function App() {
   return (
     <div>
-      <header>
-        <img className="logo" src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt=""></img>
-        <nav>
-          <ul className='nav_links'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>More</a></li>
-          </ul>
-
-        </nav>
-        <a className='cta' href='#'><button>Contact</button></a>
-      </header>
-
+      <GallaryHeader/>
       <div>
-        {
-          imageData.map(image=>{
-            return(
-              <div key={image.id} className="column">
-                <img src={image.img} alt=""></img>
-              </div>
-            )
-          })
-        }
+      <GallaryBody imageData={imageData}/>
       </div>
       {/* adding footer component */}
       <GallaryFooter/> 
